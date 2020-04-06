16c44d1f16b5eadca24f5be29e7ac053fbfba803
Nicolas Landais <nlandais@flexion.us>
2020-04-06 10:13:26 -0400

    Pass tuple with file path and file content to the upload_scan function to fix issue with Nessus scan uploads.

--- defectdojo_api/defectdojo_apiv2.py
+++ defectdojo_api/defectdojo_apiv2.py
@@ -702,13 +702,10 @@ class DefectDojoAPIv2(object):
         )
 
     ##### Upload API #####
-
-    def upload_scan(self, engagement_id, scan_type, file, active, verified, close_old_findings, skip_duplicates, scan_date, tags=None, build=None, minimum_severity="Info"):
+    def upload_scan(self, engagement_id, scan_type, filedata, active, verified, close_old_findings, skip_duplicates, scan_date, tags=None, build=None, minimum_severity="Info"):
         """Uploads and processes a scan file.
-
         :param application_id: Application identifier.
-        :param file_path: Path to the scan file to be uploaded.
-
+        :param filedata: Tuple with name and contents of (filename, open(filename, 'rb'))
         """
         if tags is None:
             tags = ''
@@ -716,9 +713,6 @@ class DefectDojoAPIv2(object):
         if build is None:
             build = ''
 
-        with open(file, 'rb') as f:
-             filedata = f.read()
-        
         if self.debug:
             print("filedata:")
             print(filedata)
