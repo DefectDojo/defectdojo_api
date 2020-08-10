abec6c3a3844c5d5a303845f99933278bb52506d
Nicolas Landais <nlandais@flexion.us>
2020-08-10 16:54:32 -0400

    Adding additional optional properties for the creation of a test

--- defectdojo_api/defectdojo_apiv2.py
+++ defectdojo_api/defectdojo_apiv2.py
@@ -399,7 +399,9 @@ class DefectDojoAPIv2(object):
         """
         return self._request('GET', 'tests/' + str(test_id) + '/')
 
-    def create_test(self, engagement_id, test_type, environment, target_start, target_end, percent_complete=None):
+    def create_test(self, engagement_id, test_type, environment, target_start,
+                    target_end, percent_complete=None, lead=None, title=None,
+                    version=None, description=None):
         """Creates a product with the given properties.
 
         :param engagement_id: Engagement id.
@@ -407,6 +409,10 @@ class DefectDojoAPIv2(object):
         :param target_start: Test start date.
         :param target_end: Test end date.
         :param percent_complete: Percentage until test completion.
+        :pram lead: Test lead id
+        :param title: Test title/name
+        :param version: Test version
+        :param description: Test description
 
         """
 
@@ -418,6 +424,18 @@ class DefectDojoAPIv2(object):
             'target_end': target_end,
             'percent_complete': percent_complete
         }
+
+        if lead:
+            data['lead'] = lead
+
+        if title:
+            data['title'] = title
+
+        if version:
+            data['version'] = version
+
+        if description:
+            data['description'] = description
 
         return self._request('POST', 'tests/', data=data)
 
